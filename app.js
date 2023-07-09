9531b17d47fd870892b0ff86396fd914f3fdfdd6
Warley Dias <warleydias@gmail.com>
2023-07-09 11:26:34 -0300

    voltando atras

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Enviando mais um comando")
-console.log("mais uma alteracao para tentar armazenar a senha e login no git bash")
