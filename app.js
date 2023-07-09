98a377d46ccdd71e30acb2ce01872f73aa26da41
Warley Dias <warleydias@gmail.com>
2023-07-09 11:06:25 -0300

    armazenar senha e login no bash

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Enviando mais um comando")
+console.log("mais uma alteracao para tentar armazenar a senha e login no git bash")
